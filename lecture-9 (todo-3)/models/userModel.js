26e8f42dd48cd223edce2b6b775bc977484d3a73
KaranSingh1301 <70521663+KaranSingh1301@users.noreply.github.com>
2024-05-20 22:47:31 +0530

    lecture 9

---
+++ lecture-9 (todo-3)/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "India",
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
+
+// Normailzation in dbms 1nf, 2nf, 3nf bcnf
